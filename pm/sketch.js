2ccb130b8cfd009af564e3c74e98bf1cb05095e4
Alexander Kaurov <kaurov@ias.edu>
2017-08-07 21:10:40 -0400

    upd

--- pm/sketch.js
+++ pm/sketch.js
@@ -2,14 +2,67 @@ var distances = [];
 var maxDistance;
 var spacer;
 var nj = require('numjs');
+var global_step;
+var img_ref;
 
 function setup() {
   N = 128;
   scale = 4
 
-  createCanvas(N*scale, N*scale);
+
+  createCanvas(windowWidth, windowHeight);
+  scale = Math.min(windowWidth/N, windowHeight/N);
   // fft = new p5.FFT();
   maxDistance = dist(width/2, height/2, width, height);
+
+  icfield();
+
+
+
+  img = createImage(width, height);
+  // img.loadPixels();
+  // for(var x = 0; x < N; x++) {
+  //   for(var y = 0; y < N; y++) {
+  //     img.set(x, y, [ic.get(x,y,0)*16+128, ic.get(x,y,0)*16+128, ic.get(x,y,0)*16+128, 255]);
+  //     // img.set(x, y, [k.get(x,y)*64+128, k.get(x,y,0)*64+128, k.get(x,y,0)*64+128, 255]);
+  //       // img.set(x, y, [128,128,255, 255]);
+  //   }
+  // }
+  // img.updatePixels();
+
+
+  // xp = nj.array([N/2, N/2]);
+  // yp = nj.array([N/4, N/2]);
+  //
+  // vxp = nj.array([-0.1, 0.1,0,0]);
+  // vyp = nj.array([0, 0,0,0]);
+
+  // for(var i = 0; i<Np; i++){
+  //   xp.set(i, Math.random() * N);
+  //   yp.set(i, Math.random() * N);
+  // }
+
+  c2i00 = nj.zeros(xp.shape)//, 'uint8')
+  c2i01 = nj.zeros(xp.shape)//, 'uint8')
+  c2i10 = nj.zeros(xp.shape)//, 'uint8')
+  c2i11 = nj.zeros(xp.shape)//, 'uint8')
+
+  y00 = nj.zeros(xp.shape)
+  y01 = nj.zeros(xp.shape)
+  y10 = nj.zeros(xp.shape)
+  y11 = nj.zeros(xp.shape)
+
+  spacer = 16;
+  global_step = 0;
+  // noLoop();  // Run once and stop
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  scale = Math.min(windowWidth/N, windowHeight/N);
+}
+
+function icfield() {
   phi0 = nj.zeros([N,N]);
 
   // generating random variables
@@ -17,6 +70,16 @@ function setup() {
   b = nj.random([N,N]);
   z0 = nj.log(a).multiply(-2).pow(0.5).multiply(nj.cos(b.multiply(2.0*3.1415)));
   z1 = nj.log(b).multiply(-2).pow(0.5).multiply(nj.cos(a.multiply(2.0*3.1415)));
+  // console.log(z0.get(50,50));
+  // console.log(img_ref.get(50,50));
+  //   for(var x = 0; x < N; x++) {
+  //     for(var y = 0; y < N; y++) {
+  //       z0.set(x,y,z0.get(x,y)-0.0015*img_ref.get(x,y)[0]);
+  //       z1.set(x,y,z1.get(x,y)-0.0015*img_ref.get(x,y)[0]);
+  //     }
+  //   }
+  //   console.log(z0.get(50,50));
+
   ic = nj.stack([z0,z1],-1)
 
   k = nj.zeros([N,N]);
@@ -128,52 +191,13 @@ for(var x = 0; x < N; x++) {
     if( yp.get(i)<0){yp.set(i, yp.get(i)+N)}
   }
 
-
-
-
-  img = createImage(width, height);
-  img.loadPixels();
-  for(var x = 0; x < N; x++) {
-    for(var y = 0; y < N; y++) {
-      img.set(x, y, [ic.get(x,y,0)*16+128, ic.get(x,y,0)*16+128, ic.get(x,y,0)*16+128, 255]);
-      // img.set(x, y, [k.get(x,y)*64+128, k.get(x,y,0)*64+128, k.get(x,y,0)*64+128, 255]);
-        // img.set(x, y, [128,128,255, 255]);
-    }
-  }
-  img.updatePixels();
-
-
-  // xp = nj.array([N/2, N/2]);
-  // yp = nj.array([N/4, N/2]);
-  //
-  // vxp = nj.array([-0.1, 0.1,0,0]);
-  // vyp = nj.array([0, 0,0,0]);
-
-  // for(var i = 0; i<Np; i++){
-  //   xp.set(i, Math.random() * N);
-  //   yp.set(i, Math.random() * N);
-  // }
-
-  c2i00 = nj.zeros(xp.shape)//, 'uint8')
-  c2i01 = nj.zeros(xp.shape)//, 'uint8')
-  c2i10 = nj.zeros(xp.shape)//, 'uint8')
-  c2i11 = nj.zeros(xp.shape)//, 'uint8')
-
-  y00 = nj.zeros(xp.shape)
-  y01 = nj.zeros(xp.shape)
-  y10 = nj.zeros(xp.shape)
-  y11 = nj.zeros(xp.shape)
-
-  spacer = 16;
-  // noLoop();  // Run once and stop
 }
 
-function Fg(xp,yp,vxp,vyp,phi,N) {
-
-
-    return fy.reshape([N,N]);
-  }
-
+function mouseClicked() {
+  global_step = 0;
+  icfield();
+  Loop();
+}
 
 
 function draw() {
@@ -185,7 +209,9 @@ function draw() {
 
   // image(a, 0, 0);
   mdens = nj.zeros(N*N)
-
+  global_step += 1;
+  // Evaluate "cloud in cell" coefficients
+  // and assign densities on a grid mdens
   for(var i = 0; i < xp.shape[0]; i++){
     x = Math.floor(xp.get(i));
     y = Math.floor(yp.get(i));
@@ -227,9 +253,13 @@ function draw() {
     mdens.set(c2i01.get(i), mdens.get(c2i01.get(i)) + y01.get(i));
     mdens.set(c2i11.get(i), mdens.get(c2i11.get(i)) + y11.get(i));
   }
+  // reshpaing mdens to 2d array
   mdens = mdens.reshape([N,N]);
 
+  // Evaluating potential
   phi0 = (nj.convolve(mdens, potential));
+
+  // Evaluating forces
   fx = nj.zeros([N,N]);
   fy = nj.zeros([N,N]);
 
@@ -244,9 +274,11 @@ function draw() {
   fy.slice(null, [i,i+1]).assign(phi0.slice(null, [0,1]).subtract(phi0.slice(null, [0,1])), false);
   fx.slice([i,i+1], null).assign(phi0.slice([0,1], null).subtract(phi0.slice([0,1], null)), false);
 
+  // Reshaping grid forces to 1D
   fx = fx.reshape(N*N);
   fy = fy.reshape(N*N);
 
+  // Forces on particles
   pax = nj.zeros(xp.shape[0]);
   pay = nj.zeros(yp.shape[0]);
 
@@ -270,17 +302,24 @@ function draw() {
     if( yp.get(i)<0){yp.set(i, yp.get(i)+N)}
 
   }
-  console.log(xp.get(0));
+  // console.log(xp.get(0));
 
+  // Update image
   img.loadPixels();
-    for (var x = 0; x < N*scale; x += 1) {
-      for (var y = 0; y < N*scale; y += 1) {
-        img.set(x, y, [0,0,0, 255]);
+    for (var x = 0; x < N; x += 1) {
+      for (var y = 0; y < N; y += 1) {
+        img.set(x, y, mdens.get(x,y,0)*128+128);
       }
     }
+    // for (var x = 0; x < xp.shape; x += 1) {
+    //   img.set(xp.get(x)*scale, yp.get(x)*scale, [255,255,255,255]);
+    // }
+    stroke(255);
     for (var x = 0; x < xp.shape; x += 1) {
-      img.set(xp.get(x)*scale, yp.get(x)*scale, [255,255,255,255]);
+      point(xp.get(x)*scale, yp.get(x)*scale);
     }
     img.updatePixels();
   image(img, 0, 0);
+
+  if(global_step>50){noLoop()}
 }
