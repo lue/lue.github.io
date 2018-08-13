7e32a39b45cfb0243bc70628fbe9a6463427b1ed
Alexander Kaurov <kaurov@ias.edu>
2018-08-13 14:11:46 -0400

    gw added

---
+++ gw/script.js
@@ -0,0 +1,124 @@
+var mic, fft, col, img, scale, scalex, f, fdot;
+var greeting;
+var message, fontsize = 10;
+
+function setup() {
+    frameRate(30);
+
+    scale=5;
+    scalex=5;
+    f = [];
+    fdot = 0;
+
+    message = "";
+    textSize(fontsize);
+
+    // fill(255,255,255);  // text is white
+    // greeting = createElement('h2', 'Try to whistle.');
+    // greeting.position(20, 5);
+    //
+    // textAlign(CENTER);
+    // textSize(50);
+
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
+    img = createImage(canvas.width, canvas.height);
+    img.loadPixels();
+    for(var x = 0; x < img.width; x++) {
+        for(var y = 0; y < img.height; y++) {
+            var a = map(0, 0, img.height, 255, 0);
+            img.set(x, y, [0, 0, 0, a]);
+        }
+    }
+    img.updatePixels();
+
+    col = 0;
+    noFill();
+
+    mic = new p5.AudioIn();
+    mic.start();
+    fft = new p5.FFT(0.01, 1024);
+    fft.setInput(mic);
+}
+
+
+
+function draw() {
+    background(255);
+
+    var spectrum = fft.analyze();
+    var nyquist = 22050;
+
+    fill(255,255,255);  // text is white
+    textSize(20);
+    var spec_max = Math.max(...spectrum.slice(6, 1024));
+    let spec_i = 6 + spectrum.slice(6, 1024).indexOf(spec_max);
+    var spec_f = 26.075*(spec_i) - 50.4; // stupid fit
+    //var temp = _.indexOf(spectrum, _.max(spectrum));
+    // text("centroid: "+round(spec_f)+" Hz " + round(spec_max), 10, 40);
+
+    image(img, 0, 0);
+    img.loadPixels();
+    for(var x = col*scalex; x < (col+1)*scalex; x++) {
+        for (var y = 0; y < img.height / scale; y++) {
+            var a = map(spectrum[y + 6], 0, img.height, 255, 0);
+            for (var i = 0; i < scale; i++) {
+                img.set(x, scale * y + i, [0, 0, 0, a]);
+            }
+        }
+    }
+    if(spec_max>190) {
+        for(var x = col*scalex; x < (col+1)*scalex; x++) {
+            for (var i = 0; i < scale; i++) {
+                img.set(x, scale * (spec_i - 6) + i, [255, 0, 0, 128]);
+            }
+        }
+        f.push(spec_f)
+    }
+    else {
+        if(f.length>5) {
+            var estim = [];
+            // var temp = 0;
+            for(var i = 1; i < f.length; i++) {
+                fdot = ((f[i]-f[i-1]) / (1.0/30.0));
+                estim.push(Math.pow(f[i], -11./5.)*Math.pow(fdot,3./5.));
+            }
+            var esum = 0;
+            var nesum = 0;
+            for(var i = 0; i < estim.length; i++) {
+                if(estim[i] > 0) {
+                    esum = esum + estim[i];
+                    nesum = nesum + 1;
+                }
+            }
+            esum = esum / nesum;
+            //https://en.wikipedia.org/wiki/Chirp_mass
+            var M = 5523 * esum;
+            var Msum = M * Math.pow(4,5/3);
+            // var M = 0;
+            // (speed of light)**3 / (gravitational constant) * (5/96*pi**(-8/3))**(3/5) in (solar mass / sec)
+            if(nesum > 0.5*(f.length-1)) {
+                message = "It sounded like something with total mass\nM = " +
+                    Math.round(Msum * 100) / 100 +
+                    " Solar masses.";
+            }
+            else {
+                message = "It sounded strange...";
+            }
+        }
+        else {
+            if(f.length>0) {
+                message = "Signal was too short... Try to be louder.";
+            }
+        }
+        f = [];
+        fdot = [];
+
+    }
+
+
+    col++;
+    if(col>img.width/scalex) col=0;
+    img.updatePixels();
+
+    text(message, 10, 20);
+}
